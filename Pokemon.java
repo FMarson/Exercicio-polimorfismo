de0bb5131ed4e76fba6bebd5501120f532a5274c
Fernando Marson <fernando.marson@gmail.com>
2024-10-02 19:42:48 +0100

    first commit

---
+++ Pokemon.java
@@ -0,0 +1,35 @@
+public class Pokemon {
+    private String nome;
+    private String classe;
+    private int indicePokedex;
+    private float altura;
+    private float peso;
+    private int vida;
+    private int resistencia;
+    private int defesa;
+    private String regiao;
+    private int velocidade;
+    private int numeroEvolucoes;
+    private int evolucaoAtual;
+
+    //construtor com todos os atributos imutáveis
+    //definir sets para os atributos mutáveis
+    //definir os gets
+
+    public void atacar(Pokemon alvo, int tipoAtaque){
+
+    }
+
+    public void sofrerDano(int dano){
+
+    }
+
+    public void curar(){
+
+    }
+
+    @Override
+    public String toString(){
+        return "info";
+    }
+}
